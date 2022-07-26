c2a7d3f6f880ef689c3d4e640b75400b39f53ed1
liguanqi <1031668709@qq.com>
2022-07-26 12:16:02 +0800

    第一次提交

---
+++ main.js
@@ -0,0 +1,17 @@
+// 创建一个注册表
+alert('请创建您的账号!');
+var z = prompt('请输入您的账号：');
+var m = prompt('请输入您的密码');
+alert(z)
+alert(m)
+
+var a,
+    b = 1,
+    c = 2;
+a = b;
+b = c;
+c = a;
+
+
+console.log(c)
+
